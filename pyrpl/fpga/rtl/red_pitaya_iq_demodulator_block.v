4d498ba04e5ebd8c3900771704b6abcb45cb568a
Leo Experiment PC <leonhard.neuhaus@gmail.com>
2016-05-24 19:21:55 +0200

    demodulator cosmetics

--- pyrpl/fpga/rtl/red_pitaya_iq_demodulator_block.v
+++ pyrpl/fpga/rtl/red_pitaya_iq_demodulator_block.v
@@ -46,23 +46,21 @@ module red_pitaya_iq_demodulator_block #(
 )
 (
     input clk_i,
-    input signed [SINBITS-1:0] sin, 
-    input signed [SINBITS-1:0] cos, 
-    input signed [INBITS-1:0] signal_i,
+    input signed  [SINBITS-1:0] sin, 
+    input signed  [SINBITS-1:0] cos, 
+    input signed  [INBITS-1:0]  signal_i,
     output signed [OUTBITS-1:0] signal1_o,            
     output signed [OUTBITS-1:0] signal2_o            
 );
 
 reg signed [INBITS-1:0] firstproduct_reg;
 always @(posedge clk_i) begin
-    firstproduct_reg <= signal_i;//$signed(signal_i);
+    firstproduct_reg <= signal_i;
 end
 
 reg signed [SINBITS+INBITS-1:0] product1;
 reg signed [SINBITS+INBITS-1:0] product2;
 always @(posedge clk_i) begin
-    //product1 <= $signed(firstproduct_reg)*$signed(sin);
-    //product2 <= $signed(firstproduct_reg)*$signed(cos);
     product1 <= firstproduct_reg * sin;
     product2 <= firstproduct_reg * cos;
 end
