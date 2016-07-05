cbb045b80aedc61a19903143b617654122b1636e
SheonChua <OMQ-SQZ@DJC-PC.lkb.upmc.fr>
2016-07-05 14:11:23 +0200

    Tuesday afternoon

--- pyrpl/models/tilt.py
+++ pyrpl/models/tilt.py
@@ -9,43 +9,43 @@ from . import *
 class TEM02FabryPerot(FabryPerot):
     export_to_parent = ['unlock', 'sweep', 'islocked',
                         'save_current_gain', 'calibrate',
-                        'lock_tilt', 'lock_transmission', 'lock']
+                        'lock_tilt', 'lock_transmission', 'lock', 'get_tilt_offset']
 
     def tilt(self, detuning):
-        return self._lorentz_slope(detuning)/0.6495 *self._parent.tilt._config.slope_sign \
+        return self._lorentz_slope_normalized(detuning)*self._parent.tilt._config.slope_sign \
                * 0.5 * (self._parent.tilt._config.max-self._parent.tilt._config.min)
 
     def transmission(self, detuning):
         return self._lorentz(detuning)*self._parent.transmission._config.max \
                + self._parent.transmission._config.min
 
-    def calibrate(self):
-        self.unlock()
-        duration = self.sweep()
-        # input signal calibration
-        for input in self.inputs.values():
-            try:
-                input._config._data["trigger_source"] = "asg1"
-                input._config._data["duration"] = duration
-                input._acquire()
-                curve, ma, mi = input.curve, input.max, input.min
-                input._config._data["trigger_source"] = "ch1_positive_edge"
-                input._config._data["threshold"] = ma*self._config.calibration_threshold
-                input._config._data["trigger_delay"] = 0
-                # input._config._data["hysteresis_ch1"] = ma / 20
-                input._config._data["duration"] = duration/self._config.calibration_zoom
-                input._config._data["timeout"] = duration*5
-                input._acquire()
-                curve, ma, mi = input.curve, input.max, input.min
-            finally:
-                # make sure to reload config file here so that the modified
-                # scope parameters are not written to config file
-                self._parent.c._load()
-            input._config["max"] = ma
-            input._config["min"] = mi
-
-        # turn off sweeps
-        self.unlock()
+    #def calibrate(self):
+    #    self.unlock()
+    #    duration = self.sweep()
+    #    # input signal calibration
+    #    for input in self.inputs.values():
+    #        try:
+    #            input._config._data["trigger_source"] = "asg1"
+    #            input._config._data["duration"] = duration
+    #            input._acquire()
+    #            curve, ma, mi = input.curve, input.max, input.min
+    #            input._config._data["trigger_source"] = "ch1_positive_edge"
+    #            input._config._data["threshold"] = ma*self._config.calibration_threshold
+    #            input._config._data["trigger_delay"] = 0
+    #            # input._config._data["hysteresis_ch1"] = ma / 20
+    #            input._config._data["duration"] = duration/self._config.calibration_zoom
+    #            input._config._data["timeout"] = duration*5
+    #            input._acquire()
+    #            curve, ma, mi = input.curve, input.max, input.min
+    #        finally:
+    #            # make sure to reload config file here so that the modified
+    #            # scope parameters are not written to config file
+    #            self._parent.c._load()
+    #        input._config["max"] = ma
+    #        input._config["min"] = mi
+    #
+    #    # turn off sweeps
+    #    self.unlock()
 
     @property
     def detuning_per_m(self):
@@ -106,16 +106,16 @@ class TEM02FabryPerot(FabryPerot):
                    offset=None,
                    factor=factor)
 
-    def lock(self, detuning=0, factor=1.0, stop=False):
-        while not self.islocked():
-            self._parent.piezo.pid.ival = self._config.lock.drift_offset
-            self.lock_transmission(factor=factor, detuning=self._config.lock.drift_detuning)
-            time.sleep(self._config.lock.drift_timeout)
-        if stop:
-            return
-        self.lock_transmission(detuning = self._config.lock.drift_detuning*0.66)
-        time.sleep(0.01)
-        return self.lock_tilt(detuning=detuning, factor=factor)
+    #def lock(self, detuning=0, factor=1.0, stop=False):
+    #    while not self.islocked():
+    #        self._parent.piezo.pid.ival = self._config.lock.drift_offset
+    #        self.lock_transmission(factor=factor, detuning=self._config.lock.drift_detuning)
+    #        time.sleep(self._config.lock.drift_timeout)
+    #    if stop:
+    #        return
+    #    self.lock_transmission(detuning = self._config.lock.drift_detuning*0.66)
+    #    time.sleep(0.01)
+    #    return self.lock_tilt(detuning=detuning, factor=factor)
 
     @property
     def relative_transmission(self):
@@ -137,3 +137,6 @@ class TEM02FabryPerot(FabryPerot):
                     self.logger.debug("Output %s is saturated!", o._name)
                     return False
         return True
+
+    def get_tilt_offset(self):
+        self.inputs["tilt"].get_offset()
