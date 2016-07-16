6bfb4056e2ac7460f21c8d98cf28b42385dcb6ff
Leo Experiment PC <leonhard.neuhaus@gmail.com>
2016-07-17 00:25:38 +0200

    extensive iir testing passes nosetests
    also tests measured iir transfer function vs continuous model (that
    model makes no assumption about the discretization step, basically only
    plots transfer function that is expected from zeros and poles + some
    delay)

--- pyrpl/test/test_modules.py
+++ pyrpl/test/test_modules.py
@@ -488,28 +488,175 @@ class TestClass(object):
                   setting, iir, error_threshold, extradelay, True
 
     def na_assertion(self, setting, module, error_threshold=0.1,
-                     extradelay=0, relative=False):
+                     extradelay=0, relative=False, mean=False, kinds=None):
         """ helper function: tests if module.transfer_function is withing
         error_threshold to the measured transfer function of the module"""
         self.r.na.input = module
         f, data, ampl = self.r.na.curve()
-        theory = module.transfer_function(f, extradelay=extradelay)
+        extrastring = str(setting)
+        if not kinds:
+            kinds = [None]
+        for kind in kinds:
+            if kind is None:
+                theory = module.transfer_function(f, extradelay=extradelay)
+                eth = error_threshold
+            else:
+                extrastring += '_'+kind+'_'
+                theory = module.transfer_function(f, extradelay=extradelay,
+                                                  kind=kind)
+                try:
+                    eth = error_threshold[kinds.index(kind)]
+                except:
+                    eth = error_threshold
+            if relative:
+                error = np.abs((data - theory)/theory)
+            else:
+                error = np.abs(data - theory)
+            if mean:
+                maxerror = np.mean(error)
+            else:
+                maxerror = np.max(error)
+            if maxerror > eth:
+                c = CurveDB.create(f, data, name='test_'+module.name
+                                            +'_'+extrastring+'_na-failed-data')
+                c.params["unittest_relative"] = relative
+                c.params["unittest_maxerror"] = maxerror
+                c.params["unittest_error_threshold"] = eth
+                c.params["unittest_setting"] = setting
+                c.save()
+                c.add_child(CurveDB.create(f, theory,
+                                name='test_'+module.name+'_na-failed-theory'))
+                c.add_child(CurveDB.create(f, error,
+                                name='test_'+module.name+'_na-failed-error'))
+                assert False, (maxerror, setting)
 
-        if relative:
-            error = np.abs((data - theory)/theory)
+
+    def test_iircomplicated_na_generator(self):
+        # this test defines a number of complicated IIR transfer functions that
+        # and tests whether the NA response of the filter corresponds to what's
+        # expected.
+        #
+        # sorry for the ugly code - the test works though
+        # if there is a problem, no need to try to understand what the code
+        # does at first (rather read the iir module code):
+        # Just check the latest new CurveDB curves and for each failed test
+        # you should find a set of curves whose names indicate the failed
+        # test, whose parameters show the error between measurement and
+        # theory, and by comparing the measurement and theory curve you
+        # should be able to figure out what went wrong in the iir filter...
+
+        extradelay = 0
+        error_threshold = 0.005  # mean relative error over the whole curve,
+                                 # values will be redifined individually
+        if self.r is None:
+            return
         else:
-            error = np.abs(data - theory)
-        maxerror = np.max(error)
-        if maxerror > error_threshold:
-            c = CurveDB.create(f, data,
-                           name='test_'+module.name+'_na-failed-data')
-            c.params["unittest_relative"] = relative
-            c.params["unittest_maxerror"] = maxerror
-            c.params["unittest_error_threshold"] = error_threshold
-            c.params["unittest_setting"] = setting
-            c.save()
-            c.add_child(CurveDB.create(f, theory,
-                            name='test_'+module.name+'_na-failed-theory'))
-            c.add_child(CurveDB.create(f, error,
-                            name='test_'+module.name+'_na-failed-error'))
-            assert False, (maxerror, setting)
+            r = self.r
+        # setup na
+        na = r.na
+        iir = r.iir
+
+        params = []
+        # setting 1
+        z, p, g, loops =(np.array([-1510.0000001 - 10101.36145285j, -1510.0000001 +
+                        10101.36145285j,
+                -2100.0000001 - 21828.90817759j, -2100.0000001 + 21828.90817759j,
+                -1000.0000001 - 30156.73583005j, -1000.0000001 + 30156.73583005j,
+                -1000.0000001 - 32063.2533145j, -1000.0000001 + 32063.2533145j,
+                -6100.0000001 - 44654.63524562j, -6100.0000001 + 44654.63524562j]),
+                 np.array([-151.00000010 - 16271.51686739j,
+                -151.00000010 + 16271.51686739j, -51.00000010 - 22342.54324816j,
+                -51.00000010 + 22342.54324816j, -10.00000010 - 30884.30406145j,
+                -10.00000010 + 30884.30406145j, -41.00000010 - 32732.52445066j,
+                -41.00000010 + 32732.52445066j, -51.00000010 - 46953.00496993j,
+                -51.00000010 + 46953.00496993j]),
+                0.03,
+                400)
+        naset = dict(start=3e3,
+                   stop=50e3,
+                   points=2001,
+                   rbw=[500, 500],
+                   avg=1,
+                   amplitude=0.05,
+                   input=iir,
+                   output_direct='off',
+                   logscale=True)
+        error_threshold = 0.05  #[0.01, 0.03] works if avg=10 in naset
+        params.append((z, p, g, loops, naset, "low_sampling", error_threshold,
+                       ['implemented', 'continuous']))
+
+        # setting 2 - minimum number of loops
+        z = [1e5j - 3e3]
+        p = [5e4j - 3e3]
+        g = 0.5
+        loops = None
+        naset = dict(start=3e3,
+               stop=10e6,
+               points = 1001,
+               rbw=[500, 500],
+               avg=1,
+               amplitude=0.05,
+               input=iir,
+               output_direct='off',
+               logscale=True)
+        error_threshold = 0.05 # large because of phase error at high freq
+        params.append((z, p, g, loops, naset, "loops_None", error_threshold,
+                       ['implemented', 'continuous']))
+
+        # setting 3 - complicated with well-defined loops (similar to 1)
+        z, p, g = (
+            np.array([-151.0000001 - 10101.36145285j, -151.0000001 +
+                    10101.36145285j,
+                   -210.0000001 - 21828.90817759j, -210.0000001 + 21828.90817759j,
+                   -100.0000001 - 30156.73583005j, -100.0000001 + 30156.73583005j,
+                   -100.0000001 - 32063.2533145j, -100.0000001 + 32063.2533145j,
+                   -610.0000001 - 44654.63524562j, -610.0000001 + 44654.63524562j]),
+            np.array([-151.00000010 - 16271.51686739j,
+                   -151.00000010 + 16271.51686739j, -51.00000010 - 22342.54324816j,
+                   -51.00000010 + 22342.54324816j, -50.00000010 - 30884.30406145j,
+                   -50.00000010 + 30884.30406145j, -41.00000010 - 32732.52445066j,
+                   -41.00000010 + 32732.52445066j, -51.00000010 - 46953.00496993j,
+                   -51.00000010 + 46953.00496993j]),
+            0.5)
+        loops = 80
+        naset = dict(start=3e3,
+                     stop=50e3,
+                     points=2501,
+                     rbw=[1000, 1000],
+                     avg=5,
+                     amplitude=0.02,
+                     input=iir,
+                     output_direct='off',
+                     logscale=True)
+        error_threshold = 0.025
+        params.append((z, p, g, loops, naset, "loops=80", error_threshold,
+                       ['implemented', 'continuous']))
+
+        # setting 4, medium complex transfer function
+        z = [-4e4j - 300, +4e4j - 300, -2e5j - 3000, +2e5j - 3000]
+        p = [-5e4j - 300, +5e4j - 300, -1e5j - 3000, +1e5j - 3000, -1e6j - 30000,
+             +1e6j - 30000, -5e5]
+        g = 1.0
+        loops = None
+        naset = dict(start=1e4,
+                     stop=500e3,
+                     points=301,
+                     rbw=1000,
+                     avg=1,
+                     amplitude=0.01,
+                     input='iir',
+                     output_direct='off',
+                     logscale=True)
+        error_threshold = [0.03, 0.04]
+        params.append((z, p, g, loops, naset, "4 - medium", error_threshold,
+                       ['implemented', 'continuous']))
+
+        # config na and iir and launch the na assertions
+        for param in params:
+            z, p, g, loops, naset, name, maxerror, kinds = param
+            r.na.setup(**naset)
+            iir.setup(z, p, g, loops=loops, input=na.iq, output_direct='off')
+            yield self.na_assertion, name, iir, maxerror, 0, True, True, kinds
+            # default arguments of na_assertion:
+            # setting, module, error_threshold=0.1,
+            # extradelay=0, relative=False, mean=False, kinds=[]
