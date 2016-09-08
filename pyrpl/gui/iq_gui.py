628b4cf06030d5257e1ea34c309dd4ae6881b249
SamuelDeleglise <samuel.deleglise@gmail.com>
2016-09-08 12:17:59 +0200

    Iq gui is BEAUTIFUL !!!

--- pyrpl/gui/iq_gui.py
+++ pyrpl/gui/iq_gui.py
@@ -36,7 +36,8 @@ class MyItem(QtGui.QLabel):
         parent.graphic_items.append(self)
         self.setStyleSheet(
             "QLabel{border: 1px solid black; border-radius: 5px; font-size: 15px; background-color:white}")
-        parent.scene.addWidget(self)
+        self.proxy = parent.scene.addWidget(self)
+        self.proxy.setZValue(2)
 
     def move_to_right_position(self):
         widget = self.parent.iq_widgets[0].properties[self.widget_name].widget
@@ -61,22 +62,33 @@ class Connection(object):
     arrow_width = 15
     margin = 15
 
-    def __init__(self, widget1, widget2, h_first, parent):
+    def __init__(self, widget1, widget2, h_first, parent, show_arrow=True):
         self.parent = parent
         self.widget_start = widget1
         self.widget_stop = widget2
+        self.h_first = h_first
+        self.show_arrow = show_arrow
+
+        self.brush = QtGui.QBrush(QtCore.Qt.black)
+        self.arrow = QtGui.QGraphicsPolygonItem()
+        self.arrow.setBrush(self.brush)
         self.pen = QtGui.QPen(QtCore.Qt.black,
-                                      5,
+                                      3,
                                       QtCore.Qt.SolidLine,
                                       QtCore.Qt.RoundCap,
                                       QtCore.Qt.RoundJoin)
 
 
         self.line1 = QtGui.QGraphicsLineItem()
+        self.line1.setPen(self.pen)
         self.line2 = QtGui.QGraphicsLineItem()
+        self.line2.setPen(self.pen)
+        self.line1.setZValue(1)
+        self.line2.setZValue(1)
 
         self.parent.scene.addItem(self.line1)
         self.parent.scene.addItem(self.line2)
+        self.parent.scene.addItem(self.arrow)
         self.adjust()
 
     def adjust(self):
@@ -84,9 +96,37 @@ class Connection(object):
         x2 = self.widget_stop.pos().x() + self.widget_stop.width() / 2
         y1 = self.widget_start.pos().y() + self.widget_start.height() / 2
         y2 = self.widget_stop.pos().y() + self.widget_stop.height() / 2
-        self.line1.setLine(x1, y1, x1, y2)
-        self.line2.setLine(x1, y2, x2, y2)
+        if self.h_first:
+            self.line1.setLine(x1, y1, x1, y2)
+            self.line2.setLine(x1, y2, x2, y2)
+        else:
+            self.line1.setLine(x1, y1, x2, y1)
+            self.line2.setLine(x2, y1, x2, y2)
+        if self.show_arrow:
+            if self.h_first:
+                x = x2 - self.widget_stop.width() / 2
+                y = y2
+                arrow = QtGui.QPolygonF(
+                    [QtCore.QPoint(x - self.margin, y - self.arrow_height / 2),
+                     QtCore.QPoint(x - self.margin, y + self.arrow_height / 2),
+                     QtCore.QPoint(x - self.margin + self.arrow_width, y)])
+            else:
+                x = x2
+                y = y2  - self.widget_stop.height() / 2
+                if y2<y1:
+                    margin = - self.margin
+                    arrow_width = - self.arrow_width
+                    y = y2 + self.widget_stop.height() / 2
+                else:
+                    margin = self.margin
+                    arrow_width = self.arrow_width
+                arrow = QtGui.QPolygonF(
+                    [QtCore.QPoint(x - self.arrow_height / 2, y - margin),
+                     QtCore.QPoint(x + self.arrow_height / 2, y - margin),
+                     QtCore.QPoint(x, y - margin + arrow_width)])
 
+            if self.show_arrow:
+                self.arrow.setPolygon(arrow)
     """
         def paint(self, painter, style, widget):
 
@@ -95,12 +135,17 @@ class Connection(object):
             painter.drawLine(QtCore.QLine(x1, y2, x2, y2))
 
             painter.setBrush(QtCore.Qt.black)
-            arrow = QtGui.QPolygonF(
-                [QtCore.QPoint(x2 - self.widget_start.width() / 2 - self.margin, y2 - self.arrow_height / 2),
-                 QtCore.QPoint(x2 - self.widget_start.width() / 2 - self.margin, y2 + self.arrow_height / 2),
-                 QtCore.QPoint(x2 - self.widget_start.width() / 2 - self.margin + self.arrow_width, y2)])
+            arrow =
             painter.drawPolygon(arrow)
     """
+
+class MyFrame(QtGui.QFrame):
+    def __init__(self ,parent):
+        super(MyFrame, self).__init__()
+        self.setStyleSheet("background-color: white;")
+        self.parent = parent
+        self.proxy = self.parent.scene.addWidget(self)
+        self.proxy.setZValue(-1)
 
 class AllIqWidgets(QtGui.QWidget):
     """
@@ -127,6 +172,7 @@ class AllIqWidgets(QtGui.QWidget):
             self.layout.setStretchFactor(widget, 0)
         self.scene = QtGui.QGraphicsScene()
         self.view = QtGui.QGraphicsView(self.scene)
+        self.view.setMinimumHeight(150)
         col = self.palette().background().color().name()
         self.view.setStyleSheet("border: 0px; background-color: " + col)
         self.layout.addWidget(self.view)
@@ -138,16 +184,26 @@ class AllIqWidgets(QtGui.QWidget):
             item.move_to_right_position()
         for conn in self.connections:
             conn.adjust()
+        iq = self.iq_widgets[0]
+
+        for index, prop in enumerate(["input", "acbandwidth", "frequency",
+                     "bandwidth", "quadrature_factor", "gain",
+                     "amplitude", "output_direct"][::2]):
+            widget = iq.properties[prop].widget
+            self.frames[index].setFixedSize(widget.width(), self.view.height())
+            self.frames[index].move(widget.x(),0)
+        self.scene.setSceneRect(QtCore.QRectF(self.view.rect()))
+
 
     def make_drawing(self):
         brush = QtGui.QBrush(QtCore.Qt.black)
 
-        row_center = 0.5
-        row_up = 0.25
-        row_down = 0.75
-        row_top = 0.1
-        row_center_up = 0.45
-        row_center_down = 0.55
+        row_center = 0.55
+        row_up = 0.3
+        row_down = 0.8
+        row_top = 0.15
+        row_center_up = 0.47
+        row_center_down = 0.63
         self.graphic_items = []
         self.input = MyLabel("input", row_center, "input", parent=self)
 
@@ -167,14 +223,14 @@ class AllIqWidgets(QtGui.QWidget):
         self.x_2 = MyLabel("gain", row_up, "X", parent=self)
         self.x_3 = MyLabel('gain', row_down, "X", parent=self)
 
-        self.plus = MyLabel("amplitude", row_up, "+", parent=self, x_offset=-30)
+        self.plus = MyLabel("amplitude", row_up, "+", parent=self, x_offset=0)
 
-        self.cte = MyLabel("amplitude", row_center_up, "Cte", parent=self, x_offset=-30)
+        self.cte = MyLabel("amplitude", row_center_up, "Cte", parent=self, x_offset=0)
 
-        self.plus_2 = MyLabel("amplitude", row_up, "+", parent=self, x_offset=30)
+        self.plus_2 = MyLabel("amplitude", row_center, "+", parent=self, x_offset=40)
 
-        self.output_direct = MyLabel("output_signal", row_center, "output_direct", parent=self)
-        self.output_signal = MyLabel("output_signal", row_top, "output_signal", parent=self)
+        self.output_direct = MyLabel("output_signal", row_center, "output\ndirect", parent=self)
+        self.output_signal = MyLabel("output_signal", row_top, "output\nsignal", parent=self)
 
         self.connections = []
         self.connect(self.input, self.high_pass)
@@ -182,27 +238,32 @@ class AllIqWidgets(QtGui.QWidget):
         self.connect(self.high_pass, self.x_cos1)
         self.connect(self.x_sin1, self.low_pass1)
         self.connect(self.x_cos1, self.low_pass2)
-        self.connect(self.low_pass1, self.na_real)
-        self.connect(self.low_pass2, self.na_imag)
-        self.connect(self.low_pass1, self.x_1)
+        self.connect(self.low_pass1, self.na_real, h_first=False)
+        self.connect(self.low_pass2, self.na_imag, h_first=False)
+        self.connect(self.low_pass1, self.x_1, h_first=False)
         self.connect(self.low_pass1, self.x_2)
         self.connect(self.low_pass2, self.x_3)
         self.connect(self.x_2, self.plus)
-        self.connect(self.plus, self.cte)
+        self.connect(self.cte, self.plus, h_first=False)
         self.connect(self.plus, self.x_sin2)
         self.connect(self.x_3, self.x_cos2)
-        self.connect(self.x_1, self.output_signal, h_first=False)
+        self.connect(self.x_1, self.output_signal)
         self.connect(self.x_sin2, self.plus_2, h_first=False)
         self.connect(self.x_cos2, self.plus_2, h_first=False)
         self.connect(self.plus_2, self.output_direct)
+        self.connect(self.output_direct, self.output_signal, h_first=False)
+
+        self.frames = [MyFrame(self) for i in range(4)]
+
 
 
     def connect(self, widget1, widget2, h_first=True):
         self.connections.append(Connection(widget1, widget2, h_first, self))
 
     def resizeEvent(self, event):
-        super(AllIqWidgets, self).resizeEvent(event)
         self.adjust_drawing()
+        super(AllIqWidgets, self).resizeEvent(event)
+
 
 
 class IqWidget(ModuleWidget):
