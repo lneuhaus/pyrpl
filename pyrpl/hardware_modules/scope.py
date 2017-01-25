90a4c1989b1552045d406b8e96932ed9ded23784
Leo <neuhaus@spectro.jussieu.fr>
2017-01-25 19:15:23 +0100

    removed scope save bug

--- pyrpl/hardware_modules/scope.py
+++ pyrpl/hardware_modules/scope.py
@@ -610,6 +610,6 @@ class Scope(HardwareModule):
                 d.update({'ch': ch,
                           'name': self.curve_name + ' ch' + str(ch)})
                 curves[ch - 1] = self._save_curve(self.times,
-                                                  datas[ch - 1],
+                                                  datas[ch],
                                                   **d)
         return curves
