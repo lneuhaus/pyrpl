3fb2894523137b7b6047a7b2dd8f7784a4fcc801
Leo <neuhaus@spectro.jussieu.fr>
2017-01-26 15:15:02 +0100

    strange commit request resolved

--- pyrpl/hardware_modules/scope.py
+++ pyrpl/hardware_modules/scope.py
@@ -610,6 +610,6 @@ class Scope(HardwareModule):
                 d.update({'ch': ch,
                           'name': self.curve_name + ' ch' + str(ch)})
                 curves[ch - 1] = self._save_curve(self.times,
-                                                  datas[chgi],
+                                                  datas[ch],
                                                   **d)
         return curves
