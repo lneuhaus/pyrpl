7e6facfce036b1a46ec22ca4ca6c49031df777c5
Leo <neuhaus@spectro.jussieu.fr>
2017-02-06 00:20:24 +0100

    custom models automatically loaded

--- pyrpl/software_modules/lockbox/models/__init__.py
+++ pyrpl/software_modules/lockbox/models/__init__.py
@@ -8,14 +8,15 @@ from .linear import *
 
 # try to import user models if applicable
 try:
+    usermodels = []
     from pyrpl import user_model_dir
     import sys, os
     sys.path.append(user_model_dir)
     for module in os.listdir(user_model_dir):
         if module == '__init__.py' or module[-3:] != '.py':
             continue
-        __import__(module[:-3], locals(), globals(), [], -1)
-        logger.debug("Loaded custom user models from %s"%module)
+        usermodels.append(__import__(module[:-3], locals(), globals(), [], -1))
+        logger.debug("Custom user models from %s were successfully imported!"%module)
     del module
 except:
     logger.warning("An error occured during the import of user model files!")
