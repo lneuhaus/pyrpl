af1796fb5134e4433a6664c74a081f420c79586e
SamuelDeleglise <samuel.deleglise@gmail.com>
2017-03-06 01:00:24 +0100

    scope unit tests pass with the new API

--- pyrpl/modules.py
+++ pyrpl/modules.py
@@ -326,6 +326,10 @@ class BaseModule(with_metaclass(ModuleMetaClass, object)):
         self._init_module()
         # enable autosave and load last state from config file
         self._autosave_active = True
+
+        # Loading the module directly after its creation can be problematic:
+        # For instance, a submodule will try to load itself even before it is
+        # attached to its parent...
         self._load_setup_attributes() # attributes are loaded but _setup() is not called
 
     def _init_module(self):
@@ -622,7 +626,7 @@ class BaseModule(with_metaclass(ModuleMetaClass, object)):
         """
         self.owner = None
 
-    def _clear(self):
+    def _kill_timers(self):
         """
         Kill timers and free resources for this module and all submodules.
         """
