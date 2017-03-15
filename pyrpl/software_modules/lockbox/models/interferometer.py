0fc78cb5364d64e5ace3b4abec782c2e22a36f9b
Leo <neuhaus@spectro.jussieu.fr>
2017-03-15 11:14:17 +0100

    PdhInterferometer

--- pyrpl/software_modules/lockbox/models/interferometer.py
+++ pyrpl/software_modules/lockbox/models/interferometer.py
@@ -10,18 +10,18 @@ class InterferometerPort1(InputDirect):
 
 class InterferometerPort2(InterferometerPort1):
     def expected_signal(self, phase):
-        return super(InterferometerPort2, self).expected_amplitude(-phase)
+        return super(InterferometerPort2, self).expected_signal(-phase)
 
 
 class Interferometer(Lockbox):
     _units = ['m', 'deg', 'rad']
-    wavelength = FloatProperty(max=10000, min=0, default=1.064)
+    wavelength = FloatProperty(max=10000, min=0, default=1.064e-6)
     _setup_attributes = ['wavelength']
     _gui_attributes = ['wavelength']
     variable = 'phase'
 
     inputs = LockboxModuleDictProperty(port1=InterferometerPort1,
-                                            port2=InterferometerPort2)
+                                       port2=InterferometerPort2)
 
     # pdh = InputPdh
     #    port1 = InterferometerPort1 # any attribute of type InputSignal will be instantiated in the model
@@ -39,3 +39,19 @@ class Interferometer(Lockbox):
         return val
     """
 
+class PdhInterferometerPort1(InputIq, InterferometerPort1):
+    def expected_signal(self, phase):
+        # proportional to the derivative of the signal
+        # i.e. sin(phase)+const. -> cos(phase)
+        return 0.5 * (self.max - self.min) * np.cos(phase)
+
+class PdhInterferometerPort2(InputIq, InterferometerPort2):
+    def expected_signal(self, phase):
+        # proportional to the derivative of the signal
+        # i.e. sin(phase) -> cos(phase) = sin(phase+pi/2)
+        return -0.5 * (self.max - self.min) * np.cos(phase)
+
+
+class PdhInterferometer(Interferometer):
+    inputs = LockboxModuleDictProperty(port1=InterferometerPort1,
+                                       pdh1=PdhInterferometerPort1)
