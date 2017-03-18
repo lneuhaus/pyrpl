843040c9fc8a299a6a17df3d439811be7aa90694
Leo <neuhaus@spectro.jussieu.fr>
2017-03-18 09:56:19 +0100

    python run_pyrpl.py config=test works now

--- run_pyrpl.py
+++ run_pyrpl.py
@@ -10,13 +10,21 @@ from pyrpl import Pyrpl
 import sys
 
 if __name__ == '__main__':
-    if len(sys.argv) > 2:
-        print("usage: python run_pyrpl.py [config_file_name]")
-    if len(sys.argv) == 1:
-        arg = None
-    else:
-        arg = sys.argv[1]
-    print("Creating Pyrpl instance with the config file" + str(arg))
-    PYRPL = Pyrpl(arg)
+    if len(sys.argv) > 3:
+        print("usage: python run_pyrpl.py [[config]=config_file_name] "
+              "[source=config_file_template] [hostname=hostname/ip]")
+    kwargs = dict()
+    for i, arg in enumerate(sys.argv):
+        print (i, arg)
+        if i == 0: # run_pyrpl.py
+            continue
+        k, v = arg.split('=', 1)
+        if v == "":
+            if i == 1:
+                kwargs["config"]= k
+        else:
+            kwargs[k] = v
+    print("Calling Pyrpl(**%s)"%str(kwargs))
+    PYRPL = Pyrpl(**kwargs)
     APP = QtGui.QApplication.instance()
     APP.exec_()
