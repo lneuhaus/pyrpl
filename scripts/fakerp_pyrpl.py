358bd0ae03f29e79c5f5978f0f31fb4d5280753b
Leo <neuhaus@spectro.jussieu.fr>
2017-03-25 16:39:29 +0100

    more file cleanup

--- scripts/fakerp_pyrpl.py
+++ scripts/fakerp_pyrpl.py
@@ -1,11 +1,12 @@
+""" script to test the software, e.g. in PyCharm debugger, without actually having a redpitaya """
+
 from pyrpl import Pyrpl
 from PyQt4 import QtGui, QtCore
 
 APP = QtGui.QApplication.instance()
 
-#p = Pyrpl(hostname="_FAKE_REDPITAYA_")
-#p = Pyrpl(config="leotests_source", hostname="_FAKE_REDPITAYA_")
-p = Pyrpl(config="ltests_source", hostname="_FAKE_REDPITAYA_")
+
+p = Pyrpl(config="localtest", hostname="_FAKE_REDPITAYA_")
 
 while True:
     APP.processEvents()
