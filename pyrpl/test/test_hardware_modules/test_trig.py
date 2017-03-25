efcc3d25aa89ccd0c4da20b55374a9282aacf10c
Leo Experiment PC <leonhard.neuhaus@gmail.com>
2017-03-25 01:22:30 +0100

    better test

--- pyrpl/test/test_hardware_modules/test_trig.py
+++ pyrpl/test/test_hardware_modules/test_trig.py
@@ -18,6 +18,7 @@ class TestScope(TestPyrpl):
 
     def test_trigger(self):
         self.asg = self.pyrpl.rp.asg0
+        # asg off, confirm trigger remains armed
         self.asg.setup(amplitude=0,
                        offset=0.5,
                        waveform='sin',
@@ -31,15 +32,33 @@ class TestScope(TestPyrpl):
                      trigger_source='pos_edge',
                      output_signal='asg0_phase')
         assert self.t.armed == True
+        # asg on, confirm trigger works
         self.asg.setup(frequency=10000.0,
                        amplitude=0.4,
                        offset=0.5,
                        waveform='sin',
                        output_direct='off')
         assert self.t.armed == False
+        # confirm that trigger outputs the right phase
         asg0phase = self.t.output_signal_to_phase(self.pyrpl.rp.sampler.trig)
         assert abs(asg0phase)<=1.0, asg0phase
+        # confirm that trigger outputs right phase for neg edge as well
         self.t.trigger_source='neg_edge'
         self.t.armed=True
         asg0phase = self.t.output_signal_to_phase(self.pyrpl.rp.sampler.trig)
         assert abs(asg0phase-180.0)<=1.0, asg0phase
+        # test auto_rearm
+        self.asg.frequency = 10e6 # do this at high frequency
+        self.t.auto_rearm = True
+        #assert self.t.armed == False  # even auto_rearm must be switched on
+        self.t.armed = True
+        # trigger should be armed most of the time (except for the cycle
+        # after triggering)
+        armed = 0
+        for i in range(100):
+            if self.t.armed:
+                armed += 1
+        assert armed >= 99, armed
+        # trigger should stay low when autorearm is off
+        self.t.auto_rearm = False
+        assert self.t.armed == False
