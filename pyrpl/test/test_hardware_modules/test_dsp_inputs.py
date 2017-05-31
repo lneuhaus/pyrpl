6bd0f58a8b46ff6482157dd97d554f6fc1112125
Leo <neuhaus@spectro.jussieu.fr>
2017-05-31 17:03:49 +0200

    unittest for inputs

---
+++ pyrpl/test/test_hardware_modules/test_dsp_inputs.py
@@ -0,0 +1,33 @@
+import logging
+logger = logging.getLogger(name=__name__)
+import time
+import numpy as np
+from time import sleep
+from PyQt4 import QtCore, QtGui
+from ..test_base import TestPyrpl
+APP = QtGui.QApplication.instance()
+
+
+class TestInput(TestPyrpl):
+    def setup(self):
+        self.p = self.pyrpl
+        self.l = self.pyrpl.lockbox
+        self.l.classname = 'Interferometer'
+
+    def teardown(self):
+        pass
+
+    def test_input(self):
+        self.p.lockbox.sequence[0].input = 'port1'
+        assert self.p.lockbox.sequence[0].input == 'port1', \
+            self.p.lockbox.sequence[0].input
+        self.p.lockbox.sequence[0].input = 'port2'
+        assert self.p.lockbox.sequence[0].input == 'port2', \
+            self.p.lockbox.sequence[0].input
+        self.p.lockbox.sequence[0].input = self.p.lockbox.inputs.port1
+        assert self.p.lockbox.sequence[0].input == 'port1', \
+            self.p.lockbox.sequence[0].input
+        self.p.rp.pid0.input = self.p.lockbox.inputs.port2
+        assert self.p.rp.pid0.input == 'lockbox.inputs.port2', self.p.rp.pid0.input
+        self.p.rp.pid0.input = self.p.lockbox.sequence[0].input
+        assert self.p.rp.pid0.input == 'lockbox.inputs.port1', self.p.rp.pid0.input
