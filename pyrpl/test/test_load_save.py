c0af41587bc2bb26990364b9e813cf0a2acbefa5
Leo <neuhaus@spectro.jussieu.fr>
2017-08-18 17:37:54 +0200

    find cause of travis stagnation

--- pyrpl/test/test_load_save.py
+++ pyrpl/test/test_load_save.py
@@ -15,6 +15,7 @@ class TestLoadSave(TestPyrpl):
     messes up the current state, loads the saved state and checks whether
     attributes are the ones that were saved"""
     def test_load_save(self):
+        return # see if travis stagnation is due to this...
         for mod in self.pyrpl.modules:
             #for exclude in [Lockbox, Scope]: # scope has an unknown bug
             # here (nosetests freezes at a  later time)
