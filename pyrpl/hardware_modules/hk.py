f71a7d56e38a2072c939f45baa39fef2a2bcf2c5
metzorff <remi.metzdorff@etu.upmc.fr>
2018-01-19 16:12:46 +0100

    temporary bug fix for output digital

--- pyrpl/hardware_modules/hk.py
+++ pyrpl/hardware_modules/hk.py
@@ -14,11 +14,11 @@ class HK(HardwareModule):
     # see http://stackoverflow.com/questions/2265402/adding-class-attributes-using-a-for-loop-in-python
     for i in range(8):
         locals()['expansion_P' + str(i)] = IORegister(0x20, 0x18, 0x10, bit=i,
-                                                      outputmode=True,
-                                                      doc="positive digital io")
+                                                  outputmode=True,
+                                                  doc="positive digital io")
         locals()['expansion_N' + str(i)] = IORegister(0x24, 0x1C, 0x14, bit=i,
-                                                      outputmode=True,
-                                                      doc="positive digital io")
+                                                  outputmode=True,
+                                                  doc="positive digital io")
 
     id = SelectRegister(0x0, doc="device ID", options={"prototype0": 0,
                                                        "release1": 1})
@@ -26,6 +26,13 @@ class HK(HardwareModule):
     led = IntRegister(0x30, doc="LED control with bits 1:8")
     # another option: access led as array of bools
     # led = [BoolRegister(0x30,bit=i,doc="LED "+str(i)) for i in range(8)]
+
+    def set_expansion_direction(self, index, val):
+        """Sets the output mode of expansion index (both for P and N expansions)"""
+        if not index in range(8):
+            raise ValueError("Index from 0 to 7 expected")
+        for name in ["expansion_P", "expansion_N"]:
+            getattr(HK, name + str(index)).direction(self, val)
 
     def _setup(self): # the function is here for its docstring to be used by the metaclass.
         """
