3f8b42cb64137c3fe3f215243e15591e01c07263
lneuhaus <leonhard.neuhaus@gmail.com>
2018-12-14 14:41:47 +0100

    cosmetic changes

--- pyrpl/software_modules/lockbox/lockbox.py
+++ pyrpl/software_modules/lockbox/lockbox.py
@@ -314,7 +314,8 @@ class Lockbox(LockboxModule):
         """
         return self._sweep()
 
-    _lock_loop = None  # this variable will store the lock loop
+    lock_loop = None  # this variable will store the lock loop
+
     def lock(self, **kwds):
         """
         Launches the full lock sequence, stage by stage until the end.
