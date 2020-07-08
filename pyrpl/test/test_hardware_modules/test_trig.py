1f8e37a78a94f4872c8db65b18ee218990386141
lneuhaus <leonhard.neuhaus@gmail.com>
2020-07-08 11:09:22 +0200

    attempt to fix test

--- pyrpl/test/test_hardware_modules/test_trig.py
+++ pyrpl/test/test_hardware_modules/test_trig.py
@@ -45,6 +45,7 @@ class TestTrig(TestPyrpl):
             assert abs(asg0phase-180.0)<=1.0, asg0phase
             # test auto_rearm
             self.asg.frequency = 10e6 # do this at high frequency
+            self.t.auto_rearm_delay = 0
             self.t.auto_rearm = True
             #assert self.t.armed == False  # even auto_rearm must be switched on
             self.t.armed = True
