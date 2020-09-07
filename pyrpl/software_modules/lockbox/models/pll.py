54c3ebf8876a77925f1f16b82ba886062f1d9079
Michael Croquette <michael.croquette@gmail.com>
2020-09-07 11:17:10 +0200

    add a register on the lockbox to control pwm0

--- pyrpl/software_modules/lockbox/models/pll.py
+++ pyrpl/software_modules/lockbox/models/pll.py
@@ -1,4 +1,5 @@
 from .. import *
+from pyrpl.attributes import *
 import numpy
 
 
@@ -203,6 +204,24 @@ class FilteredSignal(PfdErrorSignal):
                       output_signal='output_direct',
                       output_direct='off')
 
+class SlowOutputProperty(FloatProperty):
+    def __init__(self, **kwds):
+        super(SlowOutputProperty, self).__init__(**kwds)
+
+    def get_value(self, obj):
+        if obj is None:
+            return self
+        return (obj.pyrpl.rp.pid0.ival+1.)*0.9 # Ival goes from -1 to +1 while pwm goes from 0 to 1.8V
+
+    def set_value(self, obj, val):
+        if val > self.max:
+            obj._logger.warning("Coarse cannot go above max. value of %s!",
+                                self.max)
+        if val < self.min:
+            obj._logger.warning("Coarse cannot go above min. value of %s!",
+                                self.min)
+
+            obj.pyrpl.rp.pid0.ival = (val/0.9-1.)
 
 
 
@@ -211,7 +230,9 @@ class FilteredSignal(PfdErrorSignal):
 
 class Pll(Lockbox):
     wavelength = FloatProperty(max=1., min=0., default=1.064e-6, increment=1e-9)
-    _gui_attributes = ['wavelength']
+    #slow_output = PWMRegister(adress = 0)
+    slow_output = SlowOutputProperty(max=1.8, min=0., default=0, increment=1e-2)
+    _gui_attributes = ['wavelength',"slow_output"]
     _setup_attributes = _gui_attributes
 
     # management of intput/output units
