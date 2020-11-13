3d97751f71b3d614f1a6efa3d00394782f917761
lneuhaus <leonhard.neuhaus@gmail.com>
2020-11-13 10:26:56 +0100

    Update pyrpl/software_modules/lockbox/models/fabryperot.py
    Co-authored-by: Luke Helt <31250931+heltluke@users.noreply.github.com>

--- pyrpl/software_modules/lockbox/models/fabryperot.py
+++ pyrpl/software_modules/lockbox/models/fabryperot.py
@@ -170,7 +170,7 @@ class FPAnalogPdh(InputSignal, Lorentz):
     mod_freq = FrequencyProperty()
     is_locked_signal = SelectProperty(default="reflection", 
                                       options=["reflection", "transmission", "pdh"],
-                                      doc="signal to direct calls to is_locked to")
+                                      doc="signal used by is_locked")
     _setup_attributes = InputDirect._setup_attributes + ['mod_freq', 'is_locked_signal']
     _gui_attributes = InputDirect._gui_attributes + ['mod_freq', 'is_locked_signal']
 
