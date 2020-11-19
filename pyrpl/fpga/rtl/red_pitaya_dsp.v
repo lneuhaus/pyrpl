c1295696f0474a20213f770cc4463c84d8c6eaa5
Michael Croquette <michael.croquette@gmail.com>
2020-11-19 12:47:15 +0100

    Remove IIR, new PFD on all 3 PIDs

--- pyrpl/fpga/rtl/red_pitaya_dsp.v
+++ pyrpl/fpga/rtl/red_pitaya_dsp.v
@@ -365,6 +365,7 @@ end
 endgenerate
 assign trig_o = trig_signal;
 
+/*
 //IIR module 
 generate for (j = 4; j < 5; j = j+1) begin
     red_pitaya_iir_block iir (
@@ -384,36 +385,12 @@ generate for (j = 4; j < 5; j = j+1) begin
       );
 	  assign output_signal[j] = output_direct[j];
 end endgenerate
+*/
 
 
 //IQ modules
-generate for (j = 5; j < 6; j = j+1) begin
-    red_pitaya_iq_block_old 
-      iq
-      (
-	     // data
-	     .clk_i        (  clk_i          ),  // clock
-	     .rstn_i       (  rstn_i         ),  // reset - active low
-         .sync_i       (  sync[j]        ),  // syncronization of different dsp modules
-	     .dat_i        (  input_signal [j] ),  // input data
-	     .dat_o        (  output_direct[j]),  // output data
-		 .signal_o     (  output_signal[j]),  // output signal
 
-         // not using 2nd quadrature for most iq's: multipliers will be
-         // synthesized away by Vivado
-         //.signal2_o  (  output_signal[j*2]),  // output signal
-
-		 //communincation with PS
-		 .addr ( sys_addr[16-1:0] ),
-		 .wen  ( sys_wen & (sys_addr[20-1:16]==j) ),
-		 .ren  ( sys_ren & (sys_addr[20-1:16]==j) ),
-		 .ack  ( module_ack[j] ),
-		 .rdata (module_rdata[j]),
-	     .wdata (sys_wdata)
-      );
-end endgenerate
-
-generate for (j = 6; j < 7; j = j+1) begin
+generate for (j = 5; j < 7; j = j+1) begin
     red_pitaya_iq_block_new 
       iq
       (
@@ -439,10 +416,9 @@ generate for (j = 6; j < 7; j = j+1) begin
       );
 end endgenerate
 
-/*
 // IQ with two outputs
 generate for (j = 7; j < 8; j = j+1) begin
-    red_pitaya_iq_block_old   #( .QUADRATUREFILTERSTAGES(4) )
+    red_pitaya_iq_block_new   #( .QUADRATUREFILTERSTAGES(4) )
       iq_2_outputs
       (
          // data
@@ -463,5 +439,5 @@ generate for (j = 7; j < 8; j = j+1) begin
          .wdata (sys_wdata)
       );
 end endgenerate
-*/
+
 endmodule
